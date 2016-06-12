bb535c556838d9dcd01d82e03637a7bab3fbd7bb
Mary Prier <priertroyandmary@gmail.com>
2016-06-12 15:21:02 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+  def initialize
+    @name = "Troy"
+    @job = "Mechanic"
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+end
+
+  
